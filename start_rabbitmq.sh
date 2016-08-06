1e7c3f31d571f7016741adf84adb128e314d507e
Calvin <soh_calvin@yahoo.com>
2016-08-06 22:13:42 +0800

    Add docker start for rabbitmq

---
+++ start_rabbitmq.sh
@@ -0,0 +1,6 @@
+docker run -d --hostname bunny-host --name bunny -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+# Then goto 
+#            http://192.168.99.100:15672/#/
+# Login is 
+#            guest/guest
+
