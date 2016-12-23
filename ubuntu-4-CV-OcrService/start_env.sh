3e333372fc14772f74d944baf94dcd1f8799bb38
Soh <I319984@global.corp.sap>
2016-12-23 16:11:54 +0800

    Add ubuntu-4-CVMatcher

--- ubuntu-4-CV-OcrService/start_env.sh
+++ ubuntu-4-CV-OcrService/start_env.sh
@@ -3,4 +3,5 @@ cp ../../CV-OcrService/environment.yml .
 
 docker build -t ubuntu-4-ocr-service .
 
-docker run -v c:/sdrive/projects/CV-OcrService:/home/vcap/app/CV-OcrService  --rm -it --name ocr-service ubuntu-4-ocr-service
+docker run --net="host" -v c:/sdrive/projects/CV-OcrService:/home/vcap/app/CV-OcrService  --rm -it --name ocr-service ubuntu-4-ocr-service
+docker run --net="host" -p 9099:9099 -v c:/sdrive/projects/CV-OcrService:/home/vcap/app/CV-OcrService  --rm -it --name ocr-service ubuntu-4-ocr-service
