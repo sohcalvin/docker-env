a6f2b52866b3b1cf97d90e7aa38e6a76ecc089f9
Calvin Soh <calvin.soh@sap.com>
2017-08-23 14:01:49 +0800

    rabb

--- start_rabbitmq.sh
+++ start_rabbitmq.sh
@@ -1,4 +1,5 @@
 docker run -d --hostname bunny-host --name bunny -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run -d --hostname bunny-host --name bunny -p 15672:15672 -p 5672:5672 --memory="800m" --memory-swap="2g" rabbitmq:3-management
 # Then goto 
 #            http://192.168.99.100:15672/#/
 # Login is 
